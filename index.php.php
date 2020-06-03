33099f8e0531637362269624b0fc3835fcfc909d
pauline-mfr <p.monfrance@codeur.online>
2020-06-03 09:20:26 +0200

    Integration2

---
+++ index.php.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="Blueasy"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>BLUEASY</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="main">
+  <link href="https://fonts.googleapis.com/css2?family=Bitter:wght@700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <header class="container-fluid px-0" id="header">
+    <div class="row">
+      <div class="col">
+        <nav class="navbar navbar-expand py-5 bg-opacity">
+          <div class="container mid">
+            <a class="navbar-brand text-white bold" href="#">BLU<span class="cyan">E</span>ASY</a>
+            <ul class="navbar-nav">
+              <li class="nav-item pr-5">
+                <a class="nav-link active py-2 px-3 bold" href="#">HOME</a>
+              </li>
+              <li class="nav-item pr-5">
+                <a class="nav-link bold" href="#services">SERVICES</a>
+              </li>
+              <li class="nav-item pr-5">
+                <a class="nav-link bold" href="#video">FEATURES</a>
+              </li>
+              <li class="nav-item pr-5">
+                <a class="nav-link bold" href="#portfolio">PORTFOLIO</a>
+              </li>
+              <li class="nav-item pr-5">
+                <a class="nav-link bold" href="#contact">CONTACT</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+    <div class="text-center text-white">
+      <h1 class="light"><p class="p-2 mb-2 bg-opacity">“I’M LOOKING FOR THE UNEXPECTED.</p>
+      <p class="p-2 mt-2 bg-opacity">I’M LOOKING FOR THINGS I’VE NEVER SEEN BEFORE."</p></h1>
+      </div>
+  </header>
+  <main>
+
+      <!-- SERVICES-->
+
+      <section class="container-fluid py-4 bg-cyan text-white" id="services">
+        <div class="container mid py-5">
+          <h2>SERVICES</h2>
+          <div class="line mb-5"></div>
+          <div class="row">
+            <div class="col-3">
+              <img src="coffee.png" alt="coffee icon">
+              <h3 class="mt-4 mb-3">COFFEE</h3>
+              <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+            </div>
+            <div class="col-3">
+              <img src="instant.png" alt="pola icon">
+              <h3 class="mt-4 mb-3">INSTANT</h3>
+              <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+            </div>
+            <div class="col-3">
+              <img src="serious.png" alt="camera icon">
+              <h3 class="mt-4 mb-3">SERIOUS</h3>
+              <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+            </div>
+            <div class="col-3">
+              <img src="frame.png" alt="frame icon">
+              <h3 class="mt-4 mb-3">FRAME</h3>
+              <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!--PORTFOLIO-->
+
+      <section class="container-fluid py-4">
+        <div class="container mid pr-0 pl-0 py-5" id="portfolio">
+          <div class="row">
+            <div class="col">
+              <h2 class="dark-grey">PORTFOLIO</h2>
+              <div class="line mb-5 dark-grey"></div>
+            </div>
+            <div class="col text-right p-0 mt-3 bold" id="filters">
+              <a class="ml-0 py-2 px-3 active"href="#">ALL</a>
+              <a class="py-2 px-3" href="#">PANORAMAS</a>
+              <a class="py-2 px-3" href="#">PORTRAITS</a>
+              <a class="py-2 px-3" href="#">MACRO</a>
+              <a class="py-2 px-3" href="#">JOURNAL</a>
+            </div>
+          </div>
+          <div class="row mb-4 mt-5">
+            <div class="col-3 pr-3"><img src="grid1.png" alt="bridge"></div>
+            <div class="col-3"><img src="grid2.png" alt="bridge"></div>
+            <div class="col-3"><img src="grid3.png" alt="bridge"></div>
+            <div class="col-3 grid-end"><img src="grid4.png" alt="bridge"></div>
+          </div>
+          <div class="row">
+            <div class="col-3"><img src="grid5.png" alt="caban"></div>
+            <div class="col-3"><img src="grid6.png" alt="surfeur"></div>
+            <div class="col-3"><img src="grid7.png" alt="mountain"></div>
+            <div class="col-3 grid-end"><img src="grid8.png" alt="mountain"></div>
+          </div>
+        </div>
+      </section>
+
+      <!--VIDEO-->
+
+      <section class="container-fluid" id="video">
+        <div class="container mid text-white">
+          <div class="row">
+            <div class="col-5 pt-5">
+              <h2 class="pt-4">JUST DEFAULT SECTION</h2>
+              <div class="line mb-5"></div>
+              <p>Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps I love unerdwear.com. Soufflé cotton candy dessert candy ice cream wafer gummies cheesecake brownie.</p>
+              <p>Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate cake I love pudding. Ice cream I love powder pudding apple pie marshmallow. Cupcake marzipan oat cake bonbon I love candy canes toffee.</p>
+              <a href="#" class="btn py-3 px-5 mt-4 bg-trans bold">VISIT ME</a>
+            </div>
+            <div class="col-6 offset-1 py-4">
+              <iframe class="my-5" src="https://player.vimeo.com/video/75606887" width="560px" height="313px" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!--TWITTER -->
+
+      <section class="container-fluid py-4 bg-cyan" id="twitter">
+        <div class="container mid py-5 text-white">
+          <h2>TWITTER<img src="twitter_icon.png" alt="twitter icon" id="bird"></h2>
+          <div class="line mb-5"></div>
+          <div class="row">
+            <div class="col-11 light">
+              <p>Unercwear cookie liquorice. Cake donut cupcake lolilipop soufflé candy. Chocolate oat cake</br><span class="bold">@cheesecake</span> tootsie roll.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!--JOHN DOE-->
+
+      <section class="container-fluid py-5" id="john">
+        <div class="container mid py-5 text-white">
+          <h2>WHO IS JOHN DOE?</h2>
+          <div class="line mb-5"></div>
+          <div class="row">
+            <div class="col my-5">
+              <p>Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps I love unerdwear.com. Soufflé cotton candy dessert candy ice cream wafer gummies cheesecake brownie.</p>
+              <p>Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate cake I love pudding. Ice cream I love powder pudding apple pie marshmallow. Cupcake marzipan oat cake bonbon I love candy canes toffee.</p>
+            </div>
+            <div class="col">
+              <p>Pudding dessert jujubes tiramisu gingerbread croissant tiramisu applicake. Sesame snaps sugar plum cotton candy chocolate bonbon lemon drops candy canes cotton candy. Cake toffee pie bear claw pastry.</p>
+              <div class="icons mt-5">
+                <a class="p-3 mr-3" href="https://twitter.com/" target="_blank"><img src="twitter_icon.png" alt="twitter icon"></a>
+                <a class="p-3 mr-3" href="https://google.com/" target="_blank"><img src="google+_icon.png" alt="google+ icon"></a>
+                <a class="p-3 mr-3" href="https://dribbble.com/" target="_blank"><img src="drible_icon.png" alt="drible icon"></a>
+                <a class="p-3 mr-3" href="#" target="_blank"><img src="coffee_icon.png" alt="coffee icon"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!--QUOTE -->
+
+      <section class="container-fluid py-4 dark-grey" id="quote">
+        <div class="container mid py-5" >
+          <h2>TESTIMONIALS<img src="quote.png" alt="quotation mark"/></h2>
+          <div class="line mb-5"></div>
+          <div class="row">
+            <div class="col-11">
+              <p class="mb-5 light">Applicake chocolate cake wafer toffee pie soufflé wafer. Tart marshmallow waker macaroon cheesecake jelly. Gingerbread cookie soufflé sweet roll sweet roll jelly-o.</p>
+              <span class="cyan bold">- Walter White</span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- SECTION CONTACT-->
+
+      <section class="container-fluid py-4" id="contact">
+        <div class="container mid py-5 text-white">
+          <h2>CONTACT</h2>
+          <div class="line mb-5"></div>
+          <div class="row">
+            <div class="col-5">
+              <form>
+                <div class="form-group">
+                  <label class="mb-2" for="name">FULL NAME</label>
+                  <input type="text" class="form-control mb-4 bg-trans">
+                </div>
+                <div class="form-group">
+                  <label class="mb-2" for="email">EMAIL</label>
+                  <input type="email" class="form-control mb-4 bg-trans">
+                </div>
+              </div>
+              <div class="col-6 offset-1 mt-4">
+                <p>Jujubes bear claw croissant wafer sweet lemon drops brownie chocolate bar. Sweet lollipop halvah tootsie roll cake fruitcake. Chocolate cake chocolate bar gingerbread lollipop tart. Jujubes bear claw donut chupa chups.</p>
+                <p class="mt-5">Linda Newman</br>W325 State Road 123</br>Mondovi, WI(Wisconsin) 98746-54321</br>(715) 946 - 1234</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col 12">
+                <div class="form-group">
+                  <label class="text mb-2" for="message">MESSAGE</label>
+                  <input type="textarea" class="form-control w-100 mb-5 bg-trans" id="textarea">
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-4">
+                <button type="submit" class="btn py-4 w-100 bg-cyan text-white">MESSAGE</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+      <!-- FOOTER -->
+
+      <footer class="container-fluid py-4">
+        <div class="container mid py-5">
+          <div class="row align-items-center">
+            <div class="col-10">
+              <a href="#header">HOME<span class="px-2">|</span></a>
+              <a href="#services">SERVICES<span class="px-2">|</span></a>
+              <a href="#portfolio">PORTFOLIO<span class="px-2">|</span></a>
+              <a href="#contact">CONTACT</a>
+            </div>
+            <div class="col-2 text-right">
+              <p class="bold dark-grey">2013 ©</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+  </body>
+  </html>
